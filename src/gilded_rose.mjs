bd43fdb641a50c966cdbfa6ed6fc738fd0dc85f6
Aarne Rissanen <ari.outermedia.de>
2023-02-12 20:52:08 +0100

    exercise 4

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -1,10 +1,15 @@
 export class Item {
-  constructor(name, sellIn, quality) {
+  constructor(name, sellIn, quality, conjured = false) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
+    this.conjured = conjured;
   }
 }
+
+const backstagePass = "Backstage passes to a TAFKAL80ETC concert";
+const agedBrie = "Aged Brie";
+const hammer =  "Sulfuras, Hand of Ragnaros";
 
 export class Shop {
   constructor(items = []) {
@@ -12,52 +17,70 @@ export class Shop {
   }
 
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
-
+    this.items.forEach(item => updateItem(item));
     return this.items;
   }
+}
+
+function updateItem(item) {
+  if (item.name === hammer) return;
+
+  dropSellInByOne(item)
+  
+  switch (item.name){
+    case agedBrie:
+      updateBrie(item);
+      break;
+    case backstagePass:
+      updatePass(item);
+      break;
+    default:
+      updateOtherItem(item)
+  }
+}
+
+function updateBrie(item) {
+  increaseQuality(item, 2);
+}
+
+function updatePass(item) {
+  if (item.sellIn < 0) {
+    item.quality = 0;
+    return;
+  }
+
+  let qualityIncrease = 1;
+  if (item.sellIn < 10)  {
+    qualityIncrease = 2;
+  }
+  if (item.sellIn < 5) {
+    qualityIncrease = 3;
+  }
+  increaseQuality(item, qualityIncrease);
+}
+
+
+function updateOtherItem(item) {
+  degradeQuality(item)  
+}
+
+function increaseQuality(item, amount = 1) {
+  if (item.quality > 49) return;
+  item.quality = Math.min(item.quality + amount, 50);
+}
+
+function degradeQuality(item) {
+  let decrease = 1;
+  if (item.sellIn < 0) {
+    decrease *= 2;
+  }
+  if (item.conjured) {
+    decrease *= 2;
+  }
+
+  item.quality = Math.max(item.quality - decrease, 0);
+}
+
+function dropSellInByOne(item) {
+  item.sellIn = item.sellIn - 1;
 }
--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -1,10 +1,538 @@
 import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
+
+const backstagePass = "Backstage passes to a TAFKAL80ETC concert";
+const agedBrie = "Aged Brie";
+const hammer =  "Sulfuras, Hand of Ragnaros";
+
+
 describe("Gilded Rose", () => {
   it("should foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
   });
+
+  it("should not change number of items", () => {
+    const initialItems = [
+      new Item("foo", 0, 0),
+      new Item("foo", 0, 0),
+      new Item("foo", 0, 0)
+    ]
+
+    const gildedRose = new Shop(initialItems);
+    const items = gildedRose.updateQuality();
+   
+    expect(items.length).to.equal(3);
+  });
+
+  it("has zero items by default", () => {
+    const gildedRose = new Shop();
+    const items = gildedRose.updateQuality();
+   
+    expect(items.length).to.equal(0);
+  });
+});
+
+describe("foo, 0, 0", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item("foo", 0, 0)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(-1);
+  });
+
+  it("should not change quality", () => {
+    expect(item.quality).to.equal(0);
+  });
+});
+
+describe("foo, 0, 1", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item("foo", 0, 1)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(-1);
+  });
+
+  it("drops quality by 1", () => {
+    expect(item.quality).to.equal(0);
+  });
+});
+
+describe("foo, 0, 10", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item("foo", 0, 10)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(-1);
+  });
+
+  it("drops quality by 2", () => {
+    expect(item.quality).to.equal(8);
+  });
+});
+
+describe("foo, 0, 100", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item("foo", 0, 100)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(-1);
+  });
+
+  it("drops quality by 2", () => {
+    expect(item.quality).to.equal(98);
+  });
+
+});
+
+describe("foo, 1, 1", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item("foo", 1, 1)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(0);
+  });
+
+  it("quality drops by 1", () => {
+    expect(item.quality).to.equal(0);
+  });
+});
+
+describe("foo, 1, 2", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item("foo", 1, 2)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(0);
+  });
+
+  it("quality drops by 1", () => {
+    expect(item.quality).to.equal(1);
+  });
+});
+
+describe("Brie, -10, 50", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(agedBrie, -10, 50)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("sellIn drops by 1", () => {
+    expect(item.sellIn).to.equal(-11);
+  });
+
+  it("quality stays at 50", () => {
+    expect(item.quality).to.equal(50);
+  });  
+});
+
+
+describe("Brie, -10, 100", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(agedBrie, -10, 100)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("sellIn drops by 1", () => {
+    expect(item.sellIn).to.equal(-11);
+  });
+
+  it("quality stays at 100", () => {
+    expect(item.quality).to.equal(100);
+  });  
+});
+
+describe("Brie, 0, 0", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(agedBrie, 0, 0)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(-1);
+  });
+
+  it("increases quality by 2", () => {
+    expect(item.quality).to.equal(2);
+  });
+});
+
+
+describe("Brie, 0, 10", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(agedBrie, 0, 10)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(-1);
+  });
+
+  it("increasees quality by 2", () => {
+    expect(item.quality).to.equal(12);
+  });
+});
+
+describe("Brie, 100, 100", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(agedBrie, 100, 100)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(99);
+  });
+
+  it("quality stays at 100", () => {
+    expect(item.quality).to.equal(100);
+  });
+});
+
+describe("Hammer, -10, 10", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(hammer, -10, 10)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("sellIn stays at -10", () => {
+    expect(item.sellIn).to.equal(-10);
+  });
+
+  it("quality stays at 10", () => {
+    expect(item.quality).to.equal(10);
+  });
+});
+
+
+describe("Hammer, -10, 100", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(hammer, -10, 100)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("sellIn stays at -10", () => {
+    expect(item.sellIn).to.equal(-10);
+  });
+
+  it("quality stays at 10", () => {
+    expect(item.quality).to.equal(100);
+  });  
+});
+
+describe("Hammer, 0, 0", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(hammer, 0, 0)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("sellIn stays at 0", () => {
+    expect(item.sellIn).to.equal(0);
+  });
+
+  it("quality stays at 0", () => {
+    expect(item.quality).to.equal(0);
+  });
+});
+
+describe("Hammer, 0, 10", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(hammer, 0, 10)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("sellIn stays at 0", () => {
+    expect(item.sellIn).to.equal(0);
+  });
+
+  it("quality stays at 10", () => {
+    expect(item.quality).to.equal(10);
+  });
+});
+
+describe("Hammer, 1, 0", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(hammer, 1, 0)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("sellIn stays at 1", () => {
+    expect(item.sellIn).to.equal(1);
+  });
+
+  it("quality stays at 0", () => {
+    expect(item.quality).to.equal(0);
+  });
+});
+
+describe("Hammer, 1, 2", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(hammer, 1, 2)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("sellIn stays at 1", () => {
+    expect(item.sellIn).to.equal(1);
+  });
+
+  it("quality stays at 2", () => {
+    expect(item.quality).to.equal(2);
+  });
+});
+
+describe("Pass, 0, 0", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(backstagePass, 0, 0)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("should not change quality", () => {
+    expect(item.quality).to.equal(0);
+  });
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(-1);
+  });
+});
+
+describe("Pass, 0, 10", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(backstagePass, 0, 10)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(-1);
+  });
+
+  it("quality drops to zero", () => {
+    expect(item.quality).to.equal(0);
+  });
+});
+
+describe("Pass, 1, 1", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(backstagePass, 1, 1)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(0);
+  });
+
+  it("quality increases by 3", () => {
+    expect(item.quality).to.equal(4);
+  });
+});
+
+describe("Pass, 1, 48", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(backstagePass, 1, 48)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(0);
+  });
+
+  it("quality increases by 2", () => {
+    expect(item.quality).to.equal(50);
+  });
+});
+
+describe("Pass, 6, 6", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(backstagePass, 6, 6)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(5);
+  });
+
+  it("quality increases by 2", () => {
+    expect(item.quality).to.equal(8);
+  });
+});
+
+
+describe("Pass, 10, 10", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(backstagePass, 10, 10)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(9);
+  });
+
+  it("quality increases by 2", () => {
+    expect(item.quality).to.equal(12);
+  });
+});
+
+describe("Pass, 10, 49", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(backstagePass, 10, 49)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(9);
+  });
+
+  it("quality increases by 1", () => {
+    expect(item.quality).to.equal(50);
+  });
+});
+
+describe("Pass, 11, 11", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(backstagePass, 11, 11)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(10);
+  });
+
+  it("quality increases by 1", () => {
+    expect(item.quality).to.equal(12);
+  });
+});
+
+describe("Pass, 49, 49", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(backstagePass, 49, 49)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(48);
+  });
+
+  it("quality increases by 1", () => {
+    expect(item.quality).to.equal(50);
+  });
+});
+
+
+describe("Pass, 50, 50", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(backstagePass, 50, 50)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(49);
+  });
+
+  it("quality stays at 50", () => {
+    expect(item.quality).to.equal(50);
+  });
+});
+
+
+describe("Pass, 100, 100", () => {
+  let gildedRose;
+  let item;
+  beforeEach(() => {
+    gildedRose = new Shop([new Item(backstagePass, 100, 100)]);
+    item = gildedRose.updateQuality()[0];
+  })
+
+  it("drops sellIn by 1", () => {
+    expect(item.sellIn).to.equal(99);
+  });
+
+  it("quality stays at 100", () => {
+    expect(item.quality).to.equal(100);
+  });
+});
+
+describe("Conjured items", () => {
+  let gildedRose;
+  let item;
+
+  it("quality before sellIn degrades by 2", () => {
+    gildedRose = new Shop([new Item('foo', 10, 10, true)])
+    item = gildedRose.updateQuality()[0];
+
+    expect(item.quality).to.equal(8);
+  });
+
+  it("quality after sellIn degrades by 4", () => {
+    gildedRose = new Shop([new Item('foo', 0, 10, true)])
+    item = gildedRose.updateQuality()[0];
+
+    expect(item.quality).to.equal(6);
+  }); 
 });
